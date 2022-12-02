1305ce78c7430dcb6be9730f12d9109969690133
Julio Huncho <114673494+JulioStark@users.noreply.github.com>
2022-12-02 01:00:18 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+    string nome;
+
+    nome = Console.ReadLine();
+      
+    Console.WriteLine("Bem Vindo " + nome);
+    
+  }
+}
