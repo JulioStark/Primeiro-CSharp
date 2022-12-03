cd7be8a15601aec969e3b91675aedb9746539571
Julio Huncho <114673494+JulioStark@users.noreply.github.com>
2022-12-03 00:18:13 +0000

    Atualizando verificação de nota em 2 partes

--- main.cs
+++ main.cs
@@ -1,13 +1,32 @@
 using System;
 
 class Program {
-  public static void Main (string[] args) {
+  public static void Main (string[] args, Console console) {
 
-    string nome;
+    string nome, entrada;
+    Double n1, n2, media; 
+
+    Console.WriteLine("Digite o nome do aluno");
 
     nome = Console.ReadLine();
-      
-    Console.WriteLine("Bem Vindo " + nome);
+    
+    Console.WriteLine("Digite a primeira nota");
+
+    entrada = Console.ReadLine();
+
+    n1 = Double.Parse (entrada);
+    
+
+//REPETIÇÃO DO CODIGO!!!!!!!
+
+    Console.WriteLine("Digite a segunda nota");
+
+    entrada = Console.ReadLine();
+
+    n2 = Double.Parse (entrada);
+    
+    media = (n1 +n2)/2;
+        console.WriteLine("A media final do aluno(a) " + nome + " é " + media );
     
   }
 }
